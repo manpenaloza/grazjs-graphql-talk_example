69f703fc1c325e9a904ca39845a22714d36d4302
manpenaloza <manuel.penaloza@gmx.at>
2017-05-30 22:20:34 +0200

    add axios and async resolver for UserType

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,6 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
+// const _ = require('lodash');
 
 const {
   GraphQLObjectType,
@@ -8,10 +9,10 @@ const {
   GraphQLSchema, // takes in a root query and returns a GraphQL schema
 } = graphql;
 
-const users = [
-  { id: "abc1", firstName: "Bob", age: 20},
-  { id: "def2", firstName: "John", age: 35},
-];
+// const users = [
+//   { id: "abc1", firstName: "Bob", age: 20},
+//   { id: "def2", firstName: "John", age: 35},
+// ];
 
 const UserType = new GraphQLObjectType({
   name: 'User', // REQUIRED - name prop is a string naming the type
@@ -30,11 +31,9 @@ const RootQuery = new GraphQLObjectType({
       type: UserType, // ... "and I'll return you the user you're looking for"
       args: { id: { type: GraphQLString }}, // "Give me the id of the user you're looking for as an argument" ...^
       resolve(parentValue, args) { // >> so finally let's go to the database/api and get the data we are looking for
-        return _.find(users, { id: args.id });
-        // THIS IS BOOOOOOOOOORING!
-        // Let's fetch dynamic "realworld" data
-        // From a db directly, from an api? 
-        // Well. GraphQL doesn't give a shit!
+        // axios - as Promise-based lib - handles async Promise behavior
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(response => response.data);
       }
     }
   }
