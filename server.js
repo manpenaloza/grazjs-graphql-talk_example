c4acbedb8f00ff9bae44a1e291d40b0d95aad9d8
manpenaloza <manuel.penaloza@gmx.at>
2017-05-30 15:24:13 +0200

    connect graphql with express server

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true, // dev tool that allows us to make queries against our dev server
+
+}));
 
 app.listen(4000, () => {
   console.log('Listening on http://localhost:4000')
 });
+
